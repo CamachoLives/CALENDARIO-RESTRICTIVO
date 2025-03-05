108c80541073fc009d3c63a423b5575d37ec0d72
Cristian Alexander Camacho <rfcamacris@gmail.com>
2025-03-04 19:14:03 -0500

    C 04-03-25 He complementado poco a poco el formulario de edicion del usuario

--- src/app/user/pages/user-edicion/user-edicion.component.ts
+++ src/app/user/pages/user-edicion/user-edicion.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CountryService } from '../../../services/country.service';
 
 
 @Component({
@@ -11,5 +12,27 @@ import { FormsModule } from '@angular/forms';
 })
 
 export class UserEdicionComponent {
- 
+
+
+  imagePreview: string | ArrayBuffer | null = null;
+
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files[0]) {
+      const file = input.files[0];
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        this.imagePreview = reader.result;
+      };
+
+      reader.readAsDataURL(file);
+    }
+  }
+
+  removeImage(): void {
+    this.imagePreview = null;
+  }
+  
+
 }
