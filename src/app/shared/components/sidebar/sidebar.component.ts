e25c6414c7c112759e4cf98933879b084a86f299
Cristian Alexander Camacho <rfcamacris@gmail.com>
2025-03-06 18:40:24 -0500

    C 06/03/25 Lo he venido convirtiendo en responsive la aplicacion

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,16 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common'; // Importa esto
 
 
 @Component({
     selector: 'app-sidebar',
     standalone: true,
-    imports: [RouterLink, RouterLinkActive, RouterModule],
+    imports: [RouterLink, RouterLinkActive, RouterModule,CommonModule],
     templateUrl: './sidebar.component.html',
     styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
   perfil = "Cristian Camacho";
   photo = "https://github.com/CamachoLives.png"
+
+  isSidebarOpen = false; // Inicialmente oculto en móviles
+
+  toggleSidebar() {
+    console.log("Sidebar antes:", this.isSidebarOpen);
+    this.isSidebarOpen = !this.isSidebarOpen;
+    console.log("Sidebar después:", this.isSidebarOpen);
+  }
 }
