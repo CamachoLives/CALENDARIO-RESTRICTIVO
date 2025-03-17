525df2ed2ecd23c2e6934a514950551dea2af912
Cristian Alexander Camacho <rfcamacris@gmail.com>
2025-03-16 19:55:01 -0500

    C 16/03/25 olvide un pedazo de la organizacion

--- src/app/Ingreso/Ingreso.module.ts
+++ src/app/Ingreso/Ingreso.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms'; 
+import { FormsModule } from '@angular/forms';
 import { LoginRoutingModule } from './Ingreso-routing.module'; // ✅ Importa el enrutador de Ingreso
 import LoginComponent from './login/login.component'; // ✅ Importa el componente standalone
 
 @NgModule({
   declarations: [], // ✅ Declara los componentes de este módulo
   imports: [
-    CommonModule, 
-    FormsModule, 
+    CommonModule,
+    FormsModule,
     LoginRoutingModule, // ✅ Importa el módulo de rutas
-    LoginComponent 
-  ] 
+    LoginComponent,
+  ],
 })
 export class IngresoModule {}
