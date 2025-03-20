ea1fc76d41ed9e7f53da15eea06736c50979f153
Cristian Alexander Camacho <rfcamacris@gmail.com>
2025-03-19 22:57:24 -0500

    C testing the code

--- src/app/business/profile/profile.component.ts
+++ src/app/business/profile/profile.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-profile',
-    standalone: true,
-    imports: [],
-    templateUrl: './profile.component.html',
-    styleUrl: './profile.component.css'
+  selector: 'app-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.css',
 })
-export default class ProfileComponent {
-
-}
+export default class ProfileComponent {}
