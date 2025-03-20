68f05e06b3b6ce29f189c0dd067e408f098de771
Cristian Alexander Camacho <rfcamacris@gmail.com>
2025-03-19 22:56:27 -0500

    C Another configuration

--- src/app/business/tables/tables.component.ts
+++ src/app/business/tables/tables.component.ts
@@ -2,14 +2,12 @@ import { Component } from '@angular/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 
 @Component({
-    selector: 'app-tables',
-    standalone: true,
-    imports: [],
-    templateUrl: './tables.component.html',
-    styleUrl: './tables.component.css'
+  selector: 'app-tables',
+  standalone: true,
+  imports: [],
+  templateUrl: './tables.component.html',
+  styleUrl: './tables.component.css',
 })
 export default class TablesComponent {
   calendarPlugins = [dayGridPlugin];
 }
-
-
