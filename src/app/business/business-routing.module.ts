165b881489536e0bc1ee9090a74059c8eb438041
Cristian Alexander Camacho <rfcamacris@gmail.com>
2025-03-19 22:58:02 -0500

    C configurate the modules

--- src/app/business/business-routing.module.ts
+++ src/app/business/business-routing.module.ts
@@ -1,24 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import DashboardComponent from './dashboard/dashboard.component'; 
+import DashboardComponent from './dashboard/dashboard.component';
 import ProfileComponent from './profile/profile.component';
 import TablesComponent from './tables/tables.component';
 import { LayoutComponent } from '../shared/components/layout/layout.component';
 
 const routes: Routes = [
   {
-    path: '', 
-    component: LayoutComponent, 
-    children: [  // ✅ Define las rutas internas dentro del layout
-      { path: 'dashboard', component: DashboardComponent },  
+    path: '',
+    component: LayoutComponent,
+    children: [
+      // ✅ Define las rutas internas dentro del layout
+      { path: 'dashboard', component: DashboardComponent },
       { path: 'profile', component: ProfileComponent },
       { path: 'tables', component: TablesComponent },
-    ]
-  }
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class BusinessRoutingModule { }
+export class BusinessRoutingModule {}
