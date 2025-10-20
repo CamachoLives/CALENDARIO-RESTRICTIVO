b82af0af16fdbb252b468f1e455916ed94e996e5
Christian Alexander Camacho <rfcamacris@gmail.com>
2025-10-20 17:11:42 -0500

    Now i have realized a new table for profile and customize the profile with new  fields into it

--- src/app/user/pages/user-consulta/user-consulta.component.ts
+++ src/app/user/pages/user-consulta/user-consulta.component.ts
@@ -10,6 +10,15 @@ import { Router } from '@angular/router';
   styleUrl: './user-consulta.component.css'
 })
 export class UserConsultaComponent {
+  
+  Name = "Cristian Camacho"
+  Company = "Incocredito"
+  Group = "Desarrollo de la Tecnologia"
+  Role = "Ingeniero de Desarrollo"
+  Email = "IntraPruebas@gmail.com"
+  Phone = "3148917721"
+  Hobbies = "Futbol, Candelear, Deporte, Estudiar"
+
   photo = "https://github.com/CamachoLives.png"
   userId ="Cris"
   constructor(private router: Router) {}
