45d57e4dd535434727d6cd5e7639168e4f02504b
Christian Alexander Camacho <rfcamacris@gmail.com>
2025-10-23 17:02:35 -0500

    i created the path of the route for intranet

--- src/app/configuracion/pages/layout/layout.component.ts
+++ src/app/configuracion/pages/layout/layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-
+import { ParametrizacionComponent } from "../parametrizacion/parametrizacion.component";
+import { Route, RouterModule } from '@angular/router';
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [ParametrizacionComponent, RouterModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
-
+  
 }
