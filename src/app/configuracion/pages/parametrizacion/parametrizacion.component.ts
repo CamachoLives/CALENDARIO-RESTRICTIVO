607e24f1a4872b4f9b26a4d9fbbf6452ac3718b3
Christian Alexander Camacho <rfcamacris@gmail.com>
2025-10-27 14:46:31 -0500

    i have implemented new components for module parameter

--- src/app/configuracion/pages/parametrizacion/parametrizacion.component.ts
+++ src/app/configuracion/pages/parametrizacion/parametrizacion.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-parametrizacion',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './parametrizacion.component.html',
   styleUrl: './parametrizacion.component.css'
 })
 export class ParametrizacionComponent {
+
+config = {
+    color: '#ffffff',
+    ruta: '',
+    idioma: 'es',
+    tiempoSesion: 30,
+    fa2: false,
+    dashboard: true,
+    carousel: true,
+    passwordPolicy: {
+      longitud: 8,
+      caducidad: 90
+    }
+  };
+
+  onLogoChange(event: any) {
+    const file = event.target.files[0];
+    console.log('Logo seleccionado:', file);
+  }
+
+  guardarConfiguracion() {
+    console.log('Configuración guardada:', this.config);
+    // Aquí puedes llamar a tu servicio para persistir la configuración
+  }
 
 }
