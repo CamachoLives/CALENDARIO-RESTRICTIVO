87fef51d2414e72a8adbe5ec29b3dfc82899f5f9
Christian Alexander Camacho <rfcamacris@gmail.com>
2025-10-27 16:57:12 -0500

    i am adding new validations for the page, to create the module parameters

--- src/app/configuracion/pages/parametrizacion/parametrizacion.component.ts
+++ src/app/configuracion/pages/parametrizacion/parametrizacion.component.ts
@@ -1,46 +1,62 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+export interface Guardado {
+  success: boolean;
+  message: string;
+  data: {
+    token: string;
+    id: number;
+  };
+}
+
 @Component({
   selector: 'app-parametrizacion',
+  standalone:true,
   imports: [FormsModule, CommonModule],
   templateUrl: './parametrizacion.component.html',
-  styleUrl: './parametrizacion.component.css'
+  styleUrl: './parametrizacion.component.css',
 })
 export class ParametrizacionComponent {
+  //Variables 
+  private apiUrl = 'http://localhost:7000/administracion/parametrizacion';
+  private readonly tokenKey = 'authToken';
+  constructor(private http: HttpClient) {}
+  isEditable = false;
 
-isEditable = false;
-
+  logo = ''
+  color = ''
+  path = ''
   toggleEdit(): void {
     this.isEditable = !this.isEditable;
   }
 
-
-
-config = {
-    color: '#ffffff',
-    ruta: '',
-    idioma: 'es',
-    tiempoSesion: 30,
-    fa2: false,
-    dashboard: true,
-    carousel: true,
-    passwordPolicy: {
-      longitud: 8,
-      caducidad: 90
-    }
-  };
-
   onLogoChange(event: any) {
     const file = event.target.files[0];
-    console.log('Logo seleccionado:', file);
+    console.log("Aqui el archiv --> ", file)
   }
 
-  guardarConfiguracion() {
-    console.log('Configuración guardada:', this.config);
-    // Aquí puedes llamar a tu servicio para persistir la configuración
+  resultados(){
+    const json = {
+      logo : this.logo,
+      color: this.color,
+      path : this.path
+    }
+    console.log("json --> ", json.color,json.logo,json.path)
   }
 
+   Save(email: string, password: string): Observable<Guardado> {
+      return this.http
+        .post<Guardado>(`${this.apiUrl}/login`, {
+          email,
+          password,
+        })
+        .pipe(catchError(this.handleError));
+    }
 
   modulos = [
     { nombre: 'Mejoramiento continuo', activo: false },
@@ -48,12 +64,30 @@ config = {
     { nombre: 'Auditorías', activo: false },
     { nombre: 'Recursos Humanos', activo: false },
     { nombre: 'SGSST', activo: false },
-    { nombre: 'Proveedores', activo: false }
+    { nombre: 'Proveedores', activo: false },
   ];
 
   toggleModulo(modulo: any): void {
     modulo.activo = !modulo.activo;
   }
 
-  
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    let errorMessage = 'Ha ocurrido un error inesperado';
+
+    if (error.error instanceof ErrorEvent) {
+      // Error del lado del cliente
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // Error del lado del servidor
+      if (error.error && error.error.message) {
+        errorMessage = error.error.message;
+      } else {
+        errorMessage = `Error ${error.status}: ${error.statusText}`;
+      }
+    }
+
+    console.error('AuthService Error:', error);
+    return throwError(() => new Error(errorMessage));
+  }
 }
